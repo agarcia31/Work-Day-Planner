ccac89dc352448a163739a32031bad229ebae416
Andy <a.garcia.atn31@gmail.com>
2023-02-21 19:37:27 -0800

    fix DRY Javascript

--- js/script.js
+++ js/script.js
@@ -2,53 +2,42 @@
 let todayDate = moment().format('MM-DD-YYYY');
 $("#currentDay").html(todayDate);
 
-$(document).ready(function () {
-    // saveBtn click listener
-    $(".saveBtn").click(function () {
-    let text = $(this).siblings(".description").val();
-    let time = $(this).closest(".time-block").attr("id");
-    localStorage.setItem(time, text);
+$(function() {
+    // Add event listener to all elements with class "saveBtn"
+    $(".saveBtn").on("click", function() {
+      const description = $(this).siblings(".description").val().trim();
+      const timeBlockId = $(this).closest(".time-block").attr("id");
+      localStorage.setItem(timeBlockId, description);
     });
-
-   
-    function timeTracker() {
-
-        let timeNow = moment().hour();
-
-        // Checks the loop to use which one right 
-        $(".time-block").each(function () {
-            let blockTime = parseInt($(this).attr("id").split("hour")[1]);
-
-            // To check the time and add the classes for background indicators
-            if (blockTime < timeNow) {
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-                $(this).addClass("past");
-            }
-            else if (blockTime === timeNow) {
-                $(this).removeClass("past");
-                $(this).removeClass("future");
-                $(this).addClass("present");
-            }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
-
-            }
-        })
+  
+    // Update time block classes based on current time
+    function updateTimeBlocks() {
+      const currentHour = moment().hour();
+      $(".time-block").each(function() {
+        const timeBlockHour = parseInt($(this).attr("id").split("hour")[1]);
+        $(this).removeClass("past present future");
+        if (timeBlockHour < currentHour) {
+          $(this).addClass("past");
+        } else if (timeBlockHour === currentHour) {
+          $(this).addClass("present");
+        } else {
+          $(this).addClass("future");
+        }
+      });
     }
-// Get item from local storage if any
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
+  
+    // Load saved items from local storage
+  // Array of hour IDs to iterate over
+const hours = ["hour8", "hour9", "hour10", "hour11", "hour12", "hour13", "hour14", "hour15", "hour16", "hour17"];
 
-    timeTracker();
-})
+// Loop over the hours and set the textarea values from local storage
+hours.forEach(hour => {
+  $(`#${hour} .description`).val(localStorage.getItem(hour));
+});
+
+  
+    // Call updateTimeBlocks on page load and every minute thereafter
+    updateTimeBlocks();
+    setInterval(updateTimeBlocks, 60000);
+  });
+  
